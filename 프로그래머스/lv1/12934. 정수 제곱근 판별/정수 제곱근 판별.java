6520c1852e5c31913830a6a2037e8679c27ecb31
JH4 <125551746+spainclub@users.noreply.github.com>
2023-04-13 00:50:19 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,10 +1,10 @@
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        long sqrRt = (long)Math.sqrt(n);
+        long sqrt = (long)Math.sqrt(n);
 
-        if (sqrRt * sqrRt == n) {
-            answer= (sqrRt + 1) * (sqrRt + 1);
+        if (sqrt * sqrt == n) {
+            answer= (sqrt + 1) * (sqrt + 1);
         }
         else {
             answer = -1;
